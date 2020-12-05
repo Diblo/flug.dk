fb1d920ec508e6fb3364f59965fddb472edc04f2
Henrik Ankersø <heank16@student.sdu.dk>
2020-12-05 20:02:17 +0100

    D

--- index.php
+++ index.php
@@ -3,10 +3,6 @@ require 'vendor/autoload.php';
 require 'libs/Calendar.php';
 require 'libs/Blog.php';
 
-$DOMAINE = (isset($_SERVER['HTTPS']) && !empty($_SERVER['HTTPS']) ? 'https' : 'http') . "://" . $_SERVER['HTTP_HOST'];
-$TITLE = "Fyns Linux User Group (FLUG)";
-$DESCRIPTION = "Fyns Linux User Group (FLUG) er en forening for dig med passion for Open Source.";
-
 $app = new \Slim\Slim();
 $app = new \Slim\Slim(array(
     'templates.path' => './templates',
@@ -17,19 +13,19 @@ $app->contentType('text/html; charset=utf-8');
 
 $app->get('/', function () use ($app) {
     $search = $app->view()->fetch('search.php', array());
-    $app->render('main.php', array('domaine' => $DOMAINE, 'defaultTitle' => $TITLE, 'defaultDescription' => $DESCRIPTION, 'searchField' => $search));
+    $app->render('main.php', array('domaine' => "http://" . $_SERVER['HTTP_HOST'], 'defaultTitle' => "Fyns Linux User Group (FLUG)", 'defaultDescription' => "Fyns Linux User Group (FLUG) er en forening for dig med passion for Open Source.", 'searchField' => $search));
 });
 
 $app->get('/blog', function () use ($app) {
     $blog = new \Libs\Blog();
     $search = $app->view()->fetch('search.php', array());
-    $app->render('blog.php', array('domaine' => $DOMAINE, 'defaultTitle' => $TITLE, 'defaultDescription' => $DESCRIPTION, 'blog' => $blog, 'searchField' => $search));
+    $app->render('blog.php', array('domaine' => "http://" . $_SERVER['HTTP_HOST'], 'defaultTitle' => "Fyns Linux User Group (FLUG)", 'defaultDescription' => "Fyns Linux User Group (FLUG) er en forening for dig med passion for Open Source.", 'blog' => $blog, 'searchField' => $search));
 });
 
 $app->get('/blog/:postTitle', function ($postTitle) use ($app) {
     $blog = new \Libs\Blog();
     $search = $app->view()->fetch('search.php', array());
-    $app->render('blogpost.php', array('domaine' => $DOMAINE, 'defaultTitle' => $TITLE, 'defaultDescription' => $DESCRIPTION, 'blog' => $blog, 'searchField' => $search, 'postTitle' => $postTitle));
+    $app->render('blogpost.php', array('domaine' => "http://" . $_SERVER['HTTP_HOST'], 'defaultTitle' => "Fyns Linux User Group (FLUG)", 'defaultDescription' => "Fyns Linux User Group (FLUG) er en forening for dig med passion for Open Source.", 'blog' => $blog, 'searchField' => $search, 'postTitle' => $postTitle));
 });
 
 $app->get('/calendar/:timeMin', function ($timeMin) use ($app) {
